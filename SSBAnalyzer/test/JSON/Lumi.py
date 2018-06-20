8764d477dad13d00ec2ad714855b6612cfe72674
sangilpark <sangilpark@knu.ac.kr>
2018-06-20 14:57:18 +0900

    cleaning

--- SSBAnalyzer/test/JSON/Lumi.py
+++
@@ -1,2 +0,0 @@
-import FWCore.PythonUtilities.LumiList as LumiList
-process.source.lumisToProcess = LumiList.LumiList(filename = 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt').getVLuminosityBlockRange()
--- SSBAnalyzer/test/JSON/Lumi_v1.py
+++
@@ -1,6 +0,0 @@
-import FWCore.PythonUtilities.LumiList as LumiList
-import FWCore.ParameterSet.Types as CfgTypes
-process.source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
-JSONfile = 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
-myLumis = LumiList.LumiList(filename = JSONfile).getCMSSWString().split(',')
-process.source.lumisToProcess.extend(myLumis)
